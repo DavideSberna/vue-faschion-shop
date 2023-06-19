8b993f7f43b2c07f9c9488c7b5486231517b54d8
taddeoneri <taddeo.neri10@gmail.com>
2023-06-19 15:27:48 +0200

    homepage

--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue";
 import axios from 'axios';
 
 export const store = reactive({
-    apiUrl: 'http://localhost:8000/api'
+    apiUrl: 'http://localhost:8000/api',
+    imgBasePath: 'http://127.0.0.1:8000/storage/'
 });
