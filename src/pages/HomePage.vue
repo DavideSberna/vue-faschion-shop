f953b2bcad6ff4e4fb9ee2d80dd93eb3718b987c
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-21 18:41:31 +0200

    add category homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,88 +1,106 @@
 <template>
-  <!-- <Loader/> -->
-  <h1>Home page</h1>
-  <div class="row justify-content-center my-4">
-      <div class="col-12 col-lg-8">
-          <div id="productCarousel" class="carousel carousel-dark slide">
-              <div class="carousel-indicators">
-                  <button type="button" v-for="(product, index) in products" :key="product.id"
-                      data-bs-target="#productCarousel" :class="{ 'active': activeSlide === index }">
-                  </button>
-              </div>
-              <div class="carousel-inner">
-                  <!-- <Transition :name="direction"> -->
-                      <div class="carousel-item active" :key="products[activeSlide]">
-                          <img :src="store.LinkImage + products[activeSlide]" class="d-block" :alt="products[activeSlide]" />
-                          <div class="carousel-caption d-none d-md-block">
-                              <h5>{{ products[activeSlide] }}</h5>
-                          </div>
-                      </div>
-                  <!-- </Transition> -->
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#productCarouselCaptions"
-                  data-bs-slide="prev" @click="prev()">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="##productCarouselCaptions"
-                  data-bs-slide="next" @click="next()">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-              </button>
-          </div>
-      </div>
-  </div>
-  <div class="row mt-5">
-    <div class="col-4" v-for="category in categories" :key="category.id">
-      <div class="card">
-        <p>{{ category.name }}</p>
-      </div>
-    </div>
-  </div>
+    <Loader v-if="loading" />
+    <section class="hero">
+        <h1>MyColors</h1>
+    </section>
+    <section class="container my-4" v-if="!loading">
+        <div class="row gy-4 gx-1 justify-content-center">
+            <div class="col-2 col-md-2 col-lg-1" v-for="(category, index) in categories" :key="category.id">
+                <div class="card">
+
+                    <div class="card-img-top h-300 p-3">
+                        <img :src="'/images/' + category.slug + '.svg'" :alt="category.name" class="image-fit">
+                    </div>
+                    <div class="card-title">
+                        <h6 class=" text-center">{{ category.name }}</h6>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="row justify-content-center my-4">
+            <div class="col-12 col-lg-8">
+                <div id="productCarousel" class="carousel carousel-dark slide">
+                    <div class="carousel-indicators">
+                        <button type="button" v-for="(product, index) in products" :key="product.id"
+                            data-bs-target="#productCarousel" :class="{ 'active': activeSlide === index }">
+                        </button>
+                    </div>
+                    <div class="carousel-inner">
+                        <Transition :name="direction">
+                            <div class="carousel-item active" :key="products[activeSlide].id">
+                                <img :src="store.imgBasePath + products[activeSlide].cover_image" class="d-block"
+                                    :alt="products[activeSlide].name" />
+
+
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{{ products[activeSlide].name }}</h5>
+                                    <!-- <p>{{ product.descrption }}</p> -->
+                                </div>
+                            </div>
+                        </Transition>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#productCarouselCaptions"
+                        data-bs-slide="prev" @click="prev()">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="##productCarouselCaptions"
+                        data-bs-slide="next" @click="next()">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <script>
-import Loader from '../components/Loader.vue';
-import axios from 'axios';
 import { store } from '../store';
+import axios from 'axios';
+import Loader from '../components/Loader.vue';
 export default {
     name: 'HomePage',
-    components:{
-      Loader,
+    components: {
+        Loader
     },
     data() {
         return {
             store,
-            loaders: true,
+            loading: true,
             products: [],
             categories: [],
-            activeSlide: 0
+            activeSlide: 0,
+            direction: 'next'
         }
     },
     methods: {
-        getData(){
-          axios.get(`${store.apiUrl}/mixed`).then((res)=> {
-            console.log(res.data.results.products);
-            console.log(res.data.results.categories);
-            this.products = res.data.results.products;
-            this.categories = res.data.results.categories;
-          });
+        getData() {
+            axios.get(`${store.apiUrl}/mixed`).then((res) => {
+                console.log(res.data.results);
+                this.products = res.data.results.products;
+                this.categories = res.data.results.categories;
+            }).catch((errors) => {
+                console.log(errors);
+            }).finally(() => {
+                this.loading = false;
+            });
         },
         next() {
-            // this.direction = 'next';
+            this.direction = 'next';
             this.activeSlide = this.activeSlide < this.products.length - 1 ? this.activeSlide + 1 : 0;
         },
         prev() {
-            // this.direction = 'prev';
+            this.direction = 'prev';
             this.activeSlide = this.activeSlide > 0 ? this.activeSlide - 1 : this.products.length - 1;
         }
     },
     mounted() {
         this.getData();
     }
-    
-    
-
 }
 </script>
 
--- src/pages/showProduct.vue
+++ src/pages/showProduct.vue
@@ -2,9 +2,9 @@
     <Loader v-if="loading" />
     <section class="container" v-if="!loading">
         <div class="mb-4">
-            <div class="container-fluid d-flex justify-content-between">
+            <!-- <div class="container-fluid d-flex justify-content-between">
                 <div class="p-4 w-25">
-                    <img :src="getImagePath" :alt="product.name" class="image-fit" />
+                    <img :src="product.cover_image" :alt="product.name" class="image-fit" />
                 </div>
 
                 <div class="p-4 w-75">
@@ -16,13 +16,43 @@
                 </router-link>
                 </div>
 
+            </div> -->
+
+
+
+
+            <div class="card bg-secondary text-white my-5">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img :src="product.cover_image" class="img-fluid rounded-start bg-white" :alt="product.name">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title text-uppercase">{{ product.name }}</h3>
+                            <hr>
+                            <p class="card-text"><span class="fw-light">Category: </span><span
+                                    class="text-capitalize fw-bold">{{ product.category.name }}</span></p>
+                            <p class="card-text fst-italic">{{ product.description }}</p>
+
+                            <div class="text-end">
+                                <small class="">
+                                    <p class="fw-lighter fst-italic">
+                                        Price: €{{ product.price }}</p>
+                                    <p class="fw-lighter fst-italic">
+                                        Brand: {{ product.brand.name }}</p>
+                                </small>
+                                <button type="button" class="btn btn-primary">BUY NOW</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <ul v-if="product.colors">
+<ul v-if="product.colors">
             <li v-for="color in product.colors" :style="{ backgroundColor: color.hex_value }">
                 {{ color.name }}
             </li>
         </ul>
+        </div>
     </section>
 </template>
 
